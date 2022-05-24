ecf832282069d6e987c1da2630a856720bddb18d
Danielle Lyle <karendlyle@gmail.com>
2022-05-24 11:49:04 +0200

    style hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,9 @@ const Hero = () => {
 	return (
 		<main>
 			<section>
-				<h1>Make remote work</h1>
+				<h1>
+					Make<br /> remote work
+				</h1>
 
 				<p>
 					Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch
@@ -13,10 +15,10 @@ const Hero = () => {
 				</p>
 
 				<button>Learn more</button>
+
+				<Clients />
 			</section>
 			<img src={hero} alt="hero" />
-
-			<Clients />
 		</main>
 	);
 };
