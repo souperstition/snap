79fd212dfcd998347222b91e5895afc14af33beb
Danielle Lyle <karendlyle@gmail.com>
2022-05-26 15:49:12 +0200

    mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { ReactComponent as Logo } from '../images/logo.svg';
 import { ReactComponent as UpArrow } from '../images/icon-arrow-up.svg';
 import { ReactComponent as DownArrow } from '../images/icon-arrow-down.svg';
 import { ReactComponent as MenuOpen } from '../images/icon-menu.svg';
-import Features from './Features';
-import React from 'react';
-import Company from './Company';
+import { ReactComponent as MenuClose } from '../images/icon-close-menu.svg';
 import Media from 'react-media';
+import Features from './Features';
+import Company from './Company';
 
 const Navbar = () => {
 	const [ featuresIsOpen, setFeaturesIsOpen ] = useState(false);
@@ -40,26 +40,49 @@ const Navbar = () => {
 		document.addEventListener('click', handleClickOutside);
 	}
 
+	const [ mobileNav, openMobileNav ] = useState(false);
+	let menuToggle = 'nav-links';
+
+	if (!mobileNav) {
+		menuToggle = 'nav-links hidden';
+	} else {
+		menuToggle = 'nav-links';
+	}
+
+	const handleMobileClick = () => {
+		openMobileNav(!mobileNav);
+	};
+
 	return (
 		<nav>
 			<Logo className="logo" />
-			<Media queries={{ mobile: '(max-width: 650px)' }}>{matches => matches.mobile && <MenuOpen />}</Media>
-			<ul>
-				<li className="features" onClick={handleFeaturesClick}>
-					Features {featuresIsOpen ? <UpArrow /> : <DownArrow />}
-					{featuresIsOpen && <Features />}
-				</li>
-				<li className="company" onClick={handleCompanyClick}>
-					Company {companyIsOpen ? <UpArrow /> : <DownArrow />}
-					{companyIsOpen && <Company />}
-				</li>
-				<li>Careers</li>
-				<li>About</li>
-			</ul>
-			<div className="users">
-				<button>Login</button>
-				<button className="register">Register</button>
-			</div>
+			<Media queries={{ mobile: '(max-width: 650px)' }}>
+				{matches =>
+					matches.mobile &&
+					(mobileNav ? <MenuClose onClick={handleMobileClick} /> : <MenuOpen onClick={handleMobileClick} />)}
+			</Media>
+			<Media queries={{ mobile: '(max-width: 650px)' }}>
+				{matches => (
+					<section className={matches.mobile ? menuToggle : 'nav-links'}>
+						<ul>
+							<li className="features" onClick={handleFeaturesClick}>
+								Features {featuresIsOpen ? <UpArrow /> : <DownArrow />}
+								{featuresIsOpen && <Features />}
+							</li>
+							<li className="company" onClick={handleCompanyClick}>
+								Company {companyIsOpen ? <UpArrow /> : <DownArrow />}
+								{companyIsOpen && <Company />}
+							</li>
+							<li>Careers</li>
+							<li>About</li>
+						</ul>
+						<div className="users">
+							<button>Login</button>
+							<button className="register">Register</button>
+						</div>
+					</section>
+				)}
+			</Media>
 		</nav>
 	);
 };
